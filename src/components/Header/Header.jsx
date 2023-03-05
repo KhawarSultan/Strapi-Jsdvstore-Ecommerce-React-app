a90fc82c9add9da22e6c5c320545d1dd69aecaca
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-03-05 17:22:23 +0500

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { FiShoppingCart } from 'react-icons/fi';
+import { TbSearch, CgShoppingCart, TbHeart } from "react-icons/tb";
+import "./Header.scss";
+
+const Header = () => {
+    return (
+
+        <div className="main-header px-3">
+            <div className="container py-3 text-white main-header-child ">
+                <div className="  ">
+                    <span className=" list-unstyled gap-3 links">
+                        <li > <Link className="text-decoration-none text-white" to="/">Home</Link> </li>
+                        <li > <Link className="text-decoration-none text-white" to="products">Products</Link> </li>
+                        <li > <Link className="text-decoration-none text-white" to="category">Category</Link> </li>
+                    </span>
+                </div>
+                <div className="list-unstyled  ">
+                    <li > <Link className="text-decoration-none text-white" to="/"><span className="center"> <b> JSDVSTORE.</b> </span></Link> </li>
+
+                </div>
+                <div className="  ">
+                    <span className="d-flex list-unstyled gap-4 " >
+
+                        <li className=" bold">
+                            <TbSearch className="icon" />
+                        </li>
+                        <li>
+                            <TbHeart className="icon" />
+                        </li>
+                        <li className=" position-relative">
+                            <FiShoppingCart className="cart-icon" />
+                            <span className="cart-popup">5</span>
+                        </li>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+
+
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import "./Home.scss";
+const Home = () => {
+    return (
+        <div> Hello </div>
+    );
+};
+
+export default Home;
